bfcad4ca269efb5285477c181bc719c7abc81174
Tobias Tangemann <tobias@tangemann.org>
2021-04-27 23:23:31 +0200

    Cleanup resources

--- RemoteShutdown/resource.h
+++ RemoteShutdown/resource.h
@@ -1,8 +1,8 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by RemoteShutdown.rc
+// Used by Resources.rc
 
-// Nächste Standardwerte für neue Objekte
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
